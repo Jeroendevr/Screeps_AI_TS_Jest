60a9e711e60046b4759a64573ce3d3c478e23aee
Jeroendv <jeroendevries@runbox.eu>
2022-01-27 12:49:56 +0100

    maken van constructie manager

--- src/manager/constructor.ts
+++ src/manager/constructor.ts
@@ -1,1 +1,20 @@
-var Pass
+
+const constructionManager = {
+    run: function (): void {
+        const mySpawn = 'Spawn1'
+        const mySpawnObj = Game.spawns[mySpawn]
+        const roomName = mySpawnObj.room.name
+        const location: RoomPosition = find_suitable_extension_site(mySpawnObj.pos, roomName)
+        this.createExtenstions(location)
+    },
+    createExtenstions: function (location: RoomPosition): void {
+        location.createConstructionSite(STRUCTURE_EXTENSION)
+    }
+}
+
+function find_suitable_extension_site(spawn_pos: RoomPosition, roomName: string): RoomPosition {
+    let extension_site = new RoomPosition(spawn_pos.x + 1, spawn_pos.y + 1, roomName)
+    return extension_site
+}
+
+export { constructionManager }

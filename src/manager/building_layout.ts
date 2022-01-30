b1c15a143f62c6b8b5491dfc4cd282dc65a0a3f2
Jeroendv <jeroendevries@runbox.eu>
2022-01-30 15:03:32 +0100

    const to define bo

--- src/manager/building_layout.ts
+++ src/manager/building_layout.ts
@@ -1,14 +1,13 @@
 
 function building_layout(pos: RoomPosition): [number, number, string] {
     // Returns an array with position relative to the spawn where a certain structure should be placed
-    return gcl_lv2_structures()
+    return gcl_lv2_structures[0]
 
 }
 
-function gcl_lv2_structures(): [number, number, string] {
-    return [+1, -1, STRUCTURE_EXTENSION]
-}
+const gcl_lv2_structures: [number, number, string][] = [
+    [+1, -1, STRUCTURE_EXTENSION]
+]
 
-
-export { building_layout }
+export { building_layout };
 

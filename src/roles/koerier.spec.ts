93fded2a504f12a70e058df2f408dff081c8f172
Jeroendv <jeroendevries@runbox.eu>
2022-02-20 13:12:24 +0100

    de filter optie testen is ingewikkeld

--- src/roles/koerier.spec.ts
+++ src/roles/koerier.spec.ts
@@ -1,6 +1,35 @@
+import { mockInstanceOf } from 'screeps-jest'
+import { Harvester } from './harvester'
+import { Koerier } from './koerier'
+
+const harv_1 = mockInstanceOf<Harvester>({
+    memory: {
+        harvesting: true,
+        role: 'harvester'
+    }
+})
+
+const koerier_1 = mockInstanceOf<Koerier>({
+    memory: {
+        role: 'koerier'
+    }
+})
+
+const harvester_room = mockInstanceOf<Room>({
+    find: {
+        FIND_MY_CREEPS: [harv_1, koerier_1]
+    }
+})
 
 describe('Koerier role', () => {
     it('finds all the harvesters to withdraw energy from', () => {
-        // const creep = mockInstanceOf <
+        const creep = mockInstanceOf<Koerier>({
+            memory: {
+                // hauling: false,
+                role: 'koerier'
+            },
+            room: harvester_room
+        })
+
     })
 })
